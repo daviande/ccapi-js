bd1eee52de81ca2ef9bc69f4ea9e153f3d79d162
David Anderson <danderso@gmail.com>
2025-08-09 12:51:26 -0700

    feat: switch to eslint-plugin-prettier

--- ccapi-client/eslint.config.js
+++ ccapi-client/eslint.config.js
@@ -2,9 +2,11 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   ...tseslint.configs.recommended,
   {
     ignores: ["jest.config.cjs", "**/dist/**"],
