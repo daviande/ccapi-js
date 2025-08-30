09c338626c0e3e21645efdef64e3e1c0228ccfc0
David Anderson <danderso@gmail.com>
2025-08-30 09:06:52 -0700

    fix: remove ccapi-client/dist from source control

--- packages/ccapi-client/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./ccapi-client.js";
-export * from "./ccapi-sequences.js";
--- packages/ccapi-client/dist/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./ccapi-client.js";
-export * from "./ccapi-sequences.js";
-//# sourceMappingURL=index.js.map
